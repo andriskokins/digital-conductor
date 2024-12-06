8c81ba84bc2d7870b9dcf17618f72cc96192876a
psyak19 <psyak19@exmail.nottingham.ac.uk>
2024-12-06 02:54:58 +0000

    Update ticket booking Uses recursion until answer is correct

--- ticket_booking.py
+++ ticket_booking.py
@@ -7,24 +7,62 @@ from dateparser.search import search_dates
 import classifier
 from utils import query_similarity, preprocess, load_model
 
+# Load the city index
+corpus = pd.read_csv('data/gb.csv', usecols=['city'])
+settings = {
+    'analyzer': 'char_wb',
+    'ngram_range': (3, 5)
+}
 
-def book_ticket_stepped():
-    while True:
-        print("Please enter the city you are travelling from.")
+tfidf_matrix, vectorizer = load_model(corpus, 'city', False, settings, "city_names")
+
+
+def get_time(text):
+
+    ticket_date = search_dates(text, languages=['en'],
+                               settings={'PREFER_DATES_FROM': 'future', 'RELATIVE_BASE': datetime.datetime.now()})
+
+    current_day = datetime.datetime.now()
+    next_day = current_day+datetime.timedelta(days=1)
+    while (ticket_date == None):
+        print(f"Please enter a valid date and time. For example, {next_day.strftime('%d/%m/%Y')} at 10:00")
         user_input = input("> ")
+        ticket_date = search_dates(user_input, languages=['en'],
+                                   settings={'PREFER_DATES_FROM': 'future', 'RELATIVE_BASE': current_day})
+    return ticket_date
+
+
+def confidence(text):
+    # Define confidence thresholds
+    high_threshold = 0.7
+    medium_threshold = 0.5
+
+    query = preprocess(text, remove_stopwords=False)
+    similarity_scores = query_similarity(query, tfidf_matrix, vectorizer)
+    top_match = corpus.iloc[similarity_scores.index[0]]['city']
+    conf_value = similarity_scores.iloc[0]['Cosine Similarity']
+    print(f"Confidence for '{text}' matching '{top_match}': {conf_value}")
+
+    if conf_value >= high_threshold:
+        # High confidence - accept the city
+        return top_match
+    elif conf_value >= medium_threshold:
+        # Medium confidence - ask for confirmation
+        print(f"Did you mean '{top_match}'?")
+        user_response = input("> ")
+        response_class = classifier.classify_text(user_response)
+        if response_class[0] == 'positive':
+            return top_match
+        else:
+            print("Sorry, I didn't understand, enter the name again")
+            user_response = input("> ")
+            return confidence(user_response)
+    else:
+        return
 
 
 def book_ticket(user_input):
-
     classifier.train_model(split_data=False)
-
-    # Load the city index
-    corpus = pd.read_csv('data/gb.csv', usecols=['city'])
-    settings = {
-        'analyzer': 'char_wb',
-        'ngram_range': (3, 5)
-    }
-    tfidf_matrix, vectorizer = load_model(corpus, 'city', False, settings, "city_names")
 
     valid_cities = corpus['city'].tolist()
     potential_cities = []
@@ -43,47 +81,18 @@ def book_ticket(user_input):
             potential_cities.append(city)
     print(f"Potential cities: {potential_cities}")
 
-    # Define confidence thresholds
-    high_threshold = 0.7
-    medium_threshold = 0.5
-
     for city in potential_cities:
-        query = preprocess(city, remove_stopwords=False)
-        similarity_scores = query_similarity(query, tfidf_matrix, vectorizer)
-        top_match = corpus.iloc[similarity_scores.index[0]]['city']
-        confidence = similarity_scores.iloc[0]['Cosine Similarity']
-        print(f"Confidence for '{city}' matching '{top_match}': {confidence}")
-
-        # 3 Tiered confidence confirmation
-        if confidence >= high_threshold:
-            # High confidence - accept the city
-            actual_cities.append(top_match)
-        elif confidence >= medium_threshold:
-            # Medium confidence - ask for confirmation
-            print(f"Did you mean '{top_match}'?")
-            user_response = input("> ")
-            response_class = classifier.classify_text(user_response)
-            if response_class[0] == 'positive':
-                actual_cities.append(top_match)
-            else:
-                print("Please provide the city name again.")
-        else:
-            # Low confidence - unable to identify
-            # print(f"Could not confidently identify the city for '{city}'.")
-            continue
+        actual_cities.append(confidence(city))
 
     actual_cities = list(set(actual_cities) & set(valid_cities))
     print(f"Actual cities: {actual_cities}")
 
+    # if len(actual_cities) == 0:
+        # book_ticket_stepped()
+
     ticket_from = actual_cities[0]
     ticket_to = actual_cities[1]
-    ticket_date = search_dates(user_input, languages=['en'], settings={'PREFER_DATES_FROM': 'future', 'RELATIVE_BASE': datetime.datetime.now()})
-
-
-    while(ticket_date == None):
-        print(f"Please enter a valid date and time. For example, {(datetime.datetime.today()+datetime.timedelta(days=1)).strftime('%d/%m/%Y')} at 10:00")
-        user_input = input("> ")
-        ticket_date = search_dates(user_input, languages=['en'], settings={'PREFER_DATES_FROM': 'future', 'RELATIVE_BASE': datetime.datetime.now()})
+    ticket_date = get_time(user_input)
 
     ticket_date = ticket_date[0][1].strftime('%d-%m-%Y %H:%M')
     print(f"I got the following information: From {ticket_from} to {ticket_to} on {ticket_date}, is that correct?")
@@ -91,5 +100,5 @@ def book_ticket(user_input):
     response_class = classifier.classify_text(user_response)
     if response_class[0] == 'positive':
         return
-    else:
-        book_ticket_stepped()
+    # else:
+        # book_ticket_stepped()
